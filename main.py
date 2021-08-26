6e90df60af858652b1c08de73acf3da8ffd8da66
Pythonov <grringo3@gmail.com>
2021-08-26 14:47:40 +0300

    CORS FIX

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ from pydantic import BaseModel, validator, ValidationError
 # Initial config
 app = FastAPIOffline(title='Gen App')
 
-origins = [""]
+origins = ["*"]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=[""],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
